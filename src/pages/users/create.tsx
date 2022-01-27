67cef36579f3e1db603b7c3c7595d8dce650f1f1
James <jamesistranho@gmail.com>
2022-01-27 18:34:37 -0300

    feat: add create user page function to create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -14,8 +14,35 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { useForm } from "react-hook-form";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
 
 const CreateUser: React.FC = ({}) => {
+  const createUser = useMutation(async (user: User) => {
+    const response = await api.post("users", {
+      user: {
+        ...user,
+        createdAt: new Date(),
+      },
+    });
+
+    return response.data.user;
+  });
+
+  const { handleSubmit, register, formState } = useForm();
+
+  const handleCreateUser = async (values: any) => {
+    await createUser.mutateAsync(values);
+  };
+
   return (
     <Box>
       <Header />
@@ -23,7 +50,14 @@ const CreateUser: React.FC = ({}) => {
       <Flex w="100%" my="6" maxWidth={1480} mx={"auto"} px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p="8"
+          as={"form"}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size={"lg"} fontWeight={"normal"}>
             Criar Usuário
           </Heading>
@@ -32,16 +66,44 @@ const CreateUser: React.FC = ({}) => {
 
           <VStack spacing={"8"}>
             <SimpleGrid minChildWidth={"240px"} spacing={"8"} width={"100%"}>
-              <Input name="name" label="Nome Completo" />
-              <Input name="email" type={"email"} label="E-Mail" />
+              <Input
+                name="name"
+                label="Nome Completo"
+                {...register("name", {
+                  required: "Nome obrigatório",
+                })}
+                error={formState.errors}
+              />
+
+              <Input
+                name="email"
+                type={"email"}
+                label="E-Mail"
+                {...register("email", {
+                  required: "E-Mail obrigatório",
+                })}
+                error={formState.errors}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth={"240px"} spacing={"8"} width={"100%"}>
-              <Input name="password" type={"password"} label="Senha" />
+              <Input
+                name="password"
+                type={"password"}
+                label="Senha"
+                {...register("password", {
+                  required: "Senha obrigatório",
+                })}
+                error={formState.errors}
+              />
               <Input
                 name="password_confirmation"
                 type={"password"}
                 label="Confirmação da Senha"
+                {...register("confirmation_password", {
+                  required: "Confirmação de Senha obrigatório",
+                })}
+                error={formState.errors}
               />
             </SimpleGrid>
           </VStack>
@@ -53,7 +115,13 @@ const CreateUser: React.FC = ({}) => {
                   Cancelar
                 </Button>
               </Link>
-              <Button colorScheme={"pink"}>Salvar</Button>
+              <Button
+                colorScheme={"pink"}
+                isLoading={formState.isSubmitting}
+                type="submit"
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
